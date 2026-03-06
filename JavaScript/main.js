c67330ef9b0e01cae8dadf8c034bc5573e1d7e0a
Abdulrahman Arafat <abnra3838@gmail.com>
2026-03-06 16:22:27 +0200

    Refactor navigation menu and add toggle functionality

---
+++ JavaScript/main.js
@@ -0,0 +1,8 @@
+let menuButton = document.getElementById("menuButton");
+let menuLinks = document.getElementById("menuLinks");
+
+menuButton.addEventListener("click", () => {
+
+    menuLinks.classList.toggle("showMenu")
+
+})
